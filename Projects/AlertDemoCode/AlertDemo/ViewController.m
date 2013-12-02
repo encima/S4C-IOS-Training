c1d42fdb36ba01719a6f490719135dd42d26b9a5
Christopher Gwilliams <encima@gmail.com>
2013-12-02 21:24:21 +0000

    initial commit

---
+++ Projects/AlertDemoCode/AlertDemo/ViewController.m
@@ -0,0 +1,87 @@
+//
+//  ViewController.m
+//  AlertDemo
+//
+//  Created by Mark Hammonds on 10/26/11.
+//  Copyright (c) 2011 CogniFORGE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)showMessage:(id)sender {
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Hello World!" 
+                                                      message:@"This is your first UIAlertview message." 
+                                                     delegate:self 
+                                            cancelButtonTitle:@"Button 1" 
+                                            otherButtonTitles:@"Button 2", @"Button 3", nil];
+    [message show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+	NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+	
+	if([title isEqualToString:@"Button 1"])
+	{
+		NSLog(@"Button 1 was selected.");
+	}
+	else if([title isEqualToString:@"Button 2"])
+	{
+		NSLog(@"Button 2 was selected.");
+	}
+	else if([title isEqualToString:@"Button 3"])
+	{
+		NSLog(@"Button 3 was selected.");
+	}	
+}
+
+@end
